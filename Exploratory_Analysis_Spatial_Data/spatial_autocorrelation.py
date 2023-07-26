f90d6fdaf059c937641067a5e33802fc1c202662
Eduardo Rojas <eduardo.rojasr@outlook.com>
2023-07-25 23:36:25 -0600

    update exploration

---
+++ Exploratory_Analysis_Spatial_Data/spatial_autocorrelation.py
@@ -0,0 +1,17 @@
+# %%
+import esda
+import pandas as pd
+import geopandas as gpd
+import libpysal as lps
+import numpy as np
+import matplotlib.pyplot as plt
+
+# %% [markdown]
+# Our data set comes from the Berlin airbnb scrape taken in April 2018. This dataframe
+# was constructed as part of the GeoPython 2018 workshop by Levi Wolf and Serge Rey. As
+# part of the workshop a geopandas data frame was constructed with one of the columns
+# reporting the median listing price of units in each neighborhood in Berlin:
+
+# %%
+
+# %%
