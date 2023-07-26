66df115a4621714e51b360619aa4b63a27da914a
Eduardo Rojas <eduardo.rojasr@outlook.com>
2023-07-26 00:28:06 -0600

    create a new gdf

--- Exploratory_Analysis_Spatial_Data/spatial_autocorrelation.py
+++ Exploratory_Analysis_Spatial_Data/spatial_autocorrelation.py
@@ -1,17 +1,29 @@
 # %%
 import esda
-import pandas as pd
 import geopandas as gpd
 import libpysal as lps
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from shapely.geometry import Point
 
 # %% [markdown]
+# This is a followup of the tutorial form PySAL [Exploratory Analysis of Spatial Data:
+# Spatial Autocorrelation](https://pysal.org/esda/notebooks/spatialautocorrelation.html)
 # Our data set comes from the Berlin airbnb scrape taken in April 2018. This dataframe
 # was constructed as part of the GeoPython 2018 workshop by Levi Wolf and Serge Rey. As
 # part of the workshop a geopandas data frame was constructed with one of the columns
 # reporting the median listing price of units in each neighborhood in Berlin:
 
 # %%
-
+gdf: gpd.GeoDataFrame = gpd.read_file('data/berlin-neighbourhoods.geojson')
 # %%
+bl_df: pd.DataFrame = pd.read_csv('data/berlin-listings.csv')
+
+# Create a geometry object from the lat and log in the csv file
+geometry: list[Point] = [Point(xy) for xy in zip(bl_df.longitude, bl_df.latitude)]
+crs: str = "EPSG:4326"
+bl_gdf = gpd.GeoDataFrame(bl_df, crs=crs, geometry=geometry) # type: ignore
+# %%
+# Check the data
+bl_gdf.plot()
